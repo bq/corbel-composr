24ece04de9744bbd9dc9bd835dc6fd5b9f25c3c0
Rubén Carrasco <rcarrasco@bq.com>
2015-03-05 17:52:48 +0100

    init and phraseloader
    Change-Id: I1232bb744b7bd15aef29fc2294cde4cd619bc02c

---
+++ routes/phrase.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express'),
+    router = express.Router(),
+    sr = require('sr');
+
+var extractDomain = function(Authorization) {
+    console.log(Authorization);
+    return 'domain';
+};
+
+/**
+ * [Create or update a phrase]
+ * @param  phrase:
+ *         {
+			  "name": "name",
+			  "method": "GET",
+			  "code": "{}"
+			}
+ * @return {promise}
+ */
+router.put('/phrase', function(req, res) {
+    var phrase = req.body || {};
+
+    phrase.url = extractDomain(req.get('Authorization')) + ':' + phrase.name;
+
+    res.send(sr.resources(process.env.PHRASES_COLLECTION, phrase.url).update(phrase));
+});
+
+module.exports = router;
