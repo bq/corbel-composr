00ac9129c8ad8dd5dc47d53e5cdf20a22fa7b68d
Rubén Carrasco <rcarrasco@bq.com>
2015-03-05 20:08:25 +0100

    first version of worker
    Change-Id: I34c52f59845bdb139f30599edea5988d926a323e

--- routes/phrase.js
+++ routes/phrase.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var express = require('express'),
-    router = express.Router(),
-    //sr = require('sr');
-    sr = {};
+    router = express.Router();
 
 var extractDomain = function(Authorization) {
+	// @todo extract domain from token
     console.log(Authorization);
     return 'domain';
 };
@@ -25,7 +24,9 @@ router.put('/phrase', function(req, res) {
 
     phrase.url = extractDomain(req.get('Authorization')) + ':' + phrase.name;
 
-    res.send(sr.resources(process.env.PHRASES_COLLECTION, phrase.url).update(phrase));
+    // @todo integrate with sr.js
+    // res.send(sr.resources(process.env.PHRASES_COLLECTION, phrase.url).update(phrase));
+    res.send('new phrase!');
 });
 
 module.exports = router;
