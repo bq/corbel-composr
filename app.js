57676c8af0680bed234099718d08103eb1f9bdfb
Rubén Carrasco <rcarrasco@bq.com>
2015-03-11 19:16:19 +0100

    connection to event bus and  new phrase model
    Change-Id: Id461297fafabd77c6f9146c109204fc43ddf6c51

--- app.js
+++ app.js
@@ -78,4 +78,30 @@ app.use(function(err, req, res) {
     });
 });
 
+// 404 handler
+app.use(function(req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', {
+            url: req.url
+        });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({
+            error: 'not_found',
+            errorDescription: 'Not found'
+        });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+    next();
+});
+
 module.exports = app;
