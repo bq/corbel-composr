716722d1c1972c45f2b039f05cc9207d052d1108
Rubén Carrasco <rcarrasco@bq.com>
2015-03-12 19:11:54 +0100

    unregister phrase and tests
    Change-Id: I8ddc3149680eba870f0ba3a4d23e889e7ac1412f

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,90 +2,108 @@
 
 var request = require('request');
 
-module.exports = function (grunt) {
-  // show elapsed time at the end
-  require('time-grunt')(grunt);
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  var reloadPort = 35729, files;
+    var reloadPort = 35729,
+        files;
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('./package.json'),
-    develop: {
-      server: {
-        file: 'bin/www'
-      }
-    },
-    jshint: {
-        options: {
-            jshintrc: '.jshintrc',
-            reporter: require('jshint-stylish')
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('./package.json'),
+        develop: {
+            server: {
+                file: 'bin/www'
+            }
         },
-        all: [
-            '**/*.js',
-            '!node_modules/**/*.js',
-            '!public/components/**/*.js'
-        ]
-    },
-    watch: {
-      options: {
-        nospawn: true,
-        livereload: reloadPort
-      },
-      server: {
-        files: [
-          'bin/www',
-          'app.js',
-          'routes/*.js'
-        ],
-        tasks: ['develop', 'delayed-livereload']
-      },
-      js: {
-        files: ['public/js/*.js'],
-        options: {
-          livereload: reloadPort
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                '**/*.js',
+                '!node_modules/**/*.js',
+                '!public/components/**/*.js'
+            ]
+        },
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: reloadPort
+            },
+            server: {
+                files: [
+                    'bin/www',
+                    'app.js',
+                    'routes/*.js'
+                ],
+                tasks: ['develop', 'delayed-livereload']
+            },
+            js: {
+                files: ['public/js/*.js'],
+                options: {
+                    livereload: reloadPort
+                }
+            },
+            css: {
+                files: [
+                    'public/css/*.css'
+                ],
+                options: {
+                    livereload: reloadPort
+                }
+            },
+            views: {
+                files: ['views/*.ejs'],
+                options: {
+                    livereload: reloadPort
+                }
+            }
+        },
+        mochaTest: { //test for nodejs app with mocha
+            tap: {
+                options: {
+                    reporter: 'tap',
+                    captureFile: 'target/test_results.dirty.tap', // Optionally capture the reporter output to a file
+                    quiet: false // Optionally suppress output to standard out (defaults to false)
+                },
+                src: ['test/test-suite.js']
+            },
+            ci: {
+                src: ['test/test-suite.js']
+            }
         }
-      },
-      css: {
-        files: [
-          'public/css/*.css'
-        ],
-        options: {
-          livereload: reloadPort
-        }
-      },
-      views: {
-        files: ['views/*.ejs'],
-        options: {
-          livereload: reloadPort
-        }
-      }
-    }
-  });
+    });
 
-  grunt.config.requires('watch.server.files');
-  files = grunt.config('watch.server.files');
-  files = grunt.file.expand(files);
+    grunt.config.requires('watch.server.files');
+    files = grunt.config('watch.server.files');
+    files = grunt.file.expand(files);
 
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
-          var reloaded = !err && res.statusCode === 200;
-          if (reloaded) {
-            grunt.log.ok('Delayed live reload successful.');
-          } else {
-            grunt.log.error('Unable to make a delayed live reload.');
-          }
-          done(reloaded);
-        });
-    }, 500);
-  });
+    grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function() {
+        var done = this.async();
+        setTimeout(function() {
+            request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','), function(err, res) {
+                var reloaded = !err && res.statusCode === 200;
+                if (reloaded) {
+                    grunt.log.ok('Delayed live reload successful.');
+                } else {
+                    grunt.log.error('Unable to make a delayed live reload.');
+                }
+                done(reloaded);
+            });
+        }, 500);
+    });
 
-  grunt.registerTask('default', [
-    'jshint',
-    'develop',
-    'watch'
-  ]);
+    grunt.registerTask('default', [
+        'jshint',
+        'develop',
+        'watch'
+    ]);
+
+    grunt.registerTask('test', [
+        'mochaTest:ci'
+    ]);
 };
