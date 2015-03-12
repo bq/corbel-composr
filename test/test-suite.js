b8c78b2cbe4589bd231058917e58166c5a67a4ac
Rubén Carrasco <rcarrasco@bq.com>
2015-03-12 19:11:54 +0100

    unregister phrase and tests
    Change-Id: I8ddc3149680eba870f0ba3a4d23e889e7ac1412f

---
+++ test/test-suite.js
@@ -0,0 +1,3 @@
+'use strict';
+
+require('./unit/phraseManager.js');
---
+++ test/unit/phraseManager.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var phraseManager = require('../../lib/phraseManager.js'),
+    chai = require('chai'),
+    expect = chai.expect,
+    assert = chai.assert,
+    express = require('express'),
+    router = express.Router();
+
+var getPhrase = function(id) {
+    var code = 'res.render(\'index\', {title: \'test\'});';
+    return {
+        id: id,
+        get: {
+            code: code
+        },
+        post: {
+            code: code
+        },
+        put: {
+            code: code
+        },
+        delete: {
+            code: code
+        }
+    };
+};
+
+describe('in phraseManager module', function() {
+
+    it('is defined and is an object', function() {
+        expect(phraseManager).to.be.an('object');
+    });
+
+    it('expected methods are available', function() {
+        expect(phraseManager).to.respondTo('registerPhrase');
+        expect(phraseManager).to.respondTo('unregisterPhrase');
+    });
+
+    describe('when registering a phrase', function() {
+
+        it('router is required', function() {
+            expect(function() {
+                phraseManager.registerPhrase();
+            }).to.throw('undefined:router');
+        });
+
+        it('phrase is required', function() {
+            expect(function() {
+                phraseManager.registerPhrase({});
+            }).to.throw('undefined:phrase');
+        });
+
+        it('it registers the expected phrases', function() {
+
+            var routerstacklength = router.stack.length;
+
+            for (var i = 0; i < 2; i++) {
+                phraseManager.registerPhrase(router, getPhrase('domain:test' + i));
+            }
+
+            assert.equal(router.stack.length, routerstacklength + 8);
+        });
+    });
+
+    describe('when unregistering a phrase', function() {
+
+        it('router is required', function() {
+            expect(function() {
+                phraseManager.unregisterPhrase();
+            }).to.throw('undefined:router');
+        });
+
+        it('url is required', function() {
+            expect(function() {
+                phraseManager.unregisterPhrase({});
+            }).to.throw('undefined:url');
+        });
+
+        it('it deletes the expected phrases', function() {
+            var routerstacklength = router.stack.length;
+            phraseManager.registerPhrase(router, getPhrase('domain:delete'));
+            assert.equal(router.stack.length, routerstacklength + 4);
+
+            routerstacklength = router.stack.length;
+            phraseManager.unregisterPhrase(router, '/domain/delete');
+            assert.equal(router.stack.length, routerstacklength - 4);
+        });
+
+    });
+});
