653f7b192a9c4f8a018e07f247dd31087a565998
Anthanh <anthanh.pham@bq.com>
2015-03-15 21:36:13 +0100

    project refactor
    Change-Id: Id8a0e5ef46cec5cb89b1ad9754c6692e63479690

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('./package.json'),
         develop: {
             server: {
-                file: 'bin/www'
+                file: 'bin/composer'
             }
         },
         jshint: {
@@ -24,9 +24,7 @@ module.exports = function(grunt) {
                 reporter: require('jshint-stylish')
             },
             all: [
-                '**/*.js',
-                '!node_modules/**/*.js',
-                '!public/components/**/*.js'
+                'src/**/*.js'
             ]
         },
         watch: {
@@ -36,28 +34,18 @@ module.exports = function(grunt) {
             },
             server: {
                 files: [
-                    'bin/www',
-                    'app.js',
-                    'routes/*.js'
+                    'bin/composer',
+                    'src/**/*.js'
                 ],
                 tasks: ['develop', 'delayed-livereload']
             },
-            js: {
-                files: ['public/js/*.js'],
-                options: {
-                    livereload: reloadPort
-                }
-            },
-            css: {
+            'public': {
                 files: [
-                    'public/css/*.css'
+                    'public/**/*.js',
+                    'public/**/*.css',
+                    'public/**/*.html',
+                    'src/**/*.ejs'
                 ],
-                options: {
-                    livereload: reloadPort
-                }
-            },
-            views: {
-                files: ['views/*.ejs'],
                 options: {
                     livereload: reloadPort
                 }
@@ -99,11 +87,13 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', [
         'jshint',
+        'test',
         'develop',
         'watch'
     ]);
 
     grunt.registerTask('test', [
+        'jshint',
         'mochaTest:ci'
     ]);
 };
--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -8,7 +8,7 @@ var bootstrap = function() {
     process.env.PHRASES_COLLECTION = 'composer:Phrase';
 
     // @todo integrate with corbel-js
-    var phrases = require('../public/mocks/phrases.json');
+    var phrases = require('../../public/mocks/phrases.json');
 
     phrases.forEach(function(phrase) {
         console.log(phrase);
