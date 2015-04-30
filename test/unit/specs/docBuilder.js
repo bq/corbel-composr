2ea43d6770385e4db572e04e9e000efee1782f26
Rubén Carrasco <rcarrasco@bq.com>
2015-04-30 17:39:16 +0200

    bug fix with url
    Change-Id: I4b03d7d7437547b34522d8684ea7a184380ebb13

--- test/unit/specs/docBuilder.js
+++ test/unit/specs/docBuilder.js
@@ -41,6 +41,8 @@ var getPhrase = function() {
 
 describe('in docBuilder', function() {
 
+    var baseUri = config['corbel.driver.options'].urlBase.replace('{{module}}', 'composr');
+
     it('is defined and is an object', function() {
         expect(docBuilder).to.be.an('object');
     });
@@ -59,7 +61,7 @@ describe('in docBuilder', function() {
         });
 
         it('it builds the expected definition', function() {
-            var expected = '#%RAML 0.8\n---\ntitle: url\nbaseUri: ' + config['corbel.driver.options'].urlBase + 'domain\n\'/url:\':\n    get:\n        description: \'This method will get all songs\'\n        queryParameters:\n            genre: {description: \'filter the songs by genre\'}\n        responses:\n            \'200\': {body: {application/json: {schema: \'{ "$schema": "http://json-schema.org/schema"}\'}, application/xml: null}}\n';
+            var expected = '#%RAML 0.8\n---\ntitle: url\nbaseUri: ' + baseUri + 'domain\n\'/url:\':\n    get:\n        description: \'This method will get all songs\'\n        queryParameters:\n            genre: {description: \'filter the songs by genre\'}\n        responses:\n            \'200\': {body: {application/json: {schema: \'{ "$schema": "http://json-schema.org/schema"}\'}, application/xml: null}}\n';
             assert.equal(expected, docBuilder.buildDefinition('domain', getPhrase()));
         });
     });
@@ -81,7 +83,7 @@ describe('in docBuilder', function() {
         it('the built definition can be loaded by raml', function(done) {
             docBuilder.load('domain', getPhrase()).then(function(result) {
             	assert.equal('url', result.title);
-            	assert.equal(config['corbel.driver.options'].urlBase + 'domain', result.baseUri);
+            	assert.equal(baseUri + 'domain', result.baseUri);
             	assert.include('HTTPS', result.protocols);
             	assert.equal('get', result.resources[0].methods[0].method);
             	done();
