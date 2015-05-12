aafa977139395b93dc190e77e34cd6e8958f028c
Anthanh <anthanh.pham@bq.com>
2015-05-12 14:25:07 +0200

    config minor fix
    Change-Id: I8d1c5685b4d7e0b74afb861d5dfdd33b0b44e760

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,16 @@
 var _ = require('lodash'),
-  fs = require('fs');
+    fs = require('fs');
 
 var env = process.env.NODE_ENV || 'development';
-var config = require('./config');
+var config = require('./config.json');
 
 
 //Check if enviroment config file exists and overwrite the defaults
-var environmentFileConfig = env+'.json';
+var environmentFileConfig = env + '.json';
 var fstat = fs.statSync(__dirname + '/' + environmentFileConfig);
-if(fstat.isFile()){
-  var envConfig = require('./' + environmentFileConfig);
-  config = _.defaults(envConfig, config);
+if (fstat.isFile()) {
+    var envConfig = require('./' + environmentFileConfig);
+    config = _.defaults(envConfig, config);
 }
 
 module.exports = config;
