79232d81d2a4de756bf652f281e8de5040571cf1
Anthanh <anthanh.pham@bq.com>
2015-05-19 18:18:45 +0200

    SILKROAD-1523 - token refresh phrase
    Change-Id: I2df61eb1503af6d9b98d7e19f935492321a08b02

--- test/integration/specs/login.js
+++ test/integration/specs/login.js
@@ -7,88 +7,97 @@ var request = require('supertest'),
 var clientData = clientUtils.getAdminClient();
 var demoAppClientData = clientUtils.getDemoClient();
 
-var clientToken, phraseClientLoginLocation, demoAppClientToken, clientLoginPhraseUrl;
+var clientToken,
+    phraseClientLoginLocation,
+    demoAppClientToken,
+    demoAppRefreshToken,
+    clientLoginPhraseUrl;
 
 function test(app) {
-    describe('Create a login phrase and use it for logging the user', function() {
+    describe.only('With Login phrases,', function() {
 
         var loginphrase = require('../../fixtures/phrases/phraseLoginClient.json');
 
         before(function(done) {
-          this.timeout(30000);
-          request(app)
-              .post('/token')
-              .send(demoAppClientData)
-              .expect(200)
-              .end(function(err, response) {
-                  expect(response).to.be.an('object');
-                  expect(response.body.data.accessToken).to.exist;
-                  clientToken = response.body.data.accessToken;
-                  done(err);
-              });
-        });
-
-        it('creates the client login phrase', function(done) {
             this.timeout(30000);
             request(app)
-                .put('/phrase')
-                .set('Authorization', clientToken)
-                .send(loginphrase)
-                .expect(204)
+                .post('/token')
+                .send(demoAppClientData)
+                .expect(200)
                 .end(function(err, response) {
-                    expect(response.headers).to.exist;
-                    phraseClientLoginLocation = response.headers['location'];
-                    expect(phraseClientLoginLocation.length).to.be.above(0);
+                    expect(response).to.be.an('object');
+                    expect(response.body.data.accessToken).to.exist;
+                    clientToken = response.body.data.accessToken;
                     done(err);
                 });
         });
 
-        it('receives a token after calling the client login phrase', function(done) {
-            var phraseEndpoint = loginphrase.url;
-            var domain = phraseClientLoginLocation.replace('phrase/', '').split('!')[0];
-            clientLoginPhraseUrl = '/' + domain + '/' + phraseEndpoint;
-            this.timeout(30000);
+        describe('client login phrase', function() {
 
-            //let's wait till corbel triggers the event to register the phrase in composr
-            //TODO: use any tool to know when it happens
-            setTimeout(function() {
-
+            it('is created correctly', function(done) {
+                this.timeout(30000);
                 request(app)
-                    .post(clientLoginPhraseUrl)
-                    .send(demoAppClientData)
-                    .expect(200)
+                    .put('/phrase')
+                    .set('Authorization', clientToken)
+                    .send(loginphrase)
+                    .expect(204)
                     .end(function(err, response) {
-                        expect(response).to.be.an('object');
-                        expect(response.body.data.accessToken).to.exist;
-                        demoAppClientToken = response.body.data.accessToken;
+                        expect(response.headers).to.exist;
+                        phraseClientLoginLocation = response.headers['location'];
+                        expect(phraseClientLoginLocation.length).to.be.above(0);
                         done(err);
                     });
+            });
 
-            }, 2000);
+            it('receives a token after calling it', function(done) {
+                var phraseEndpoint = loginphrase.url;
+                var domain = phraseClientLoginLocation.replace('phrase/', '').split('!')[0];
+                clientLoginPhraseUrl = '/' + domain + '/' + phraseEndpoint;
+                this.timeout(30000);
+
+                //let's wait till corbel triggers the event to register the phrase in composr
+                //TODO: use any tool to know when it happens
+                setTimeout(function() {
+
+                    request(app)
+                        .post(clientLoginPhraseUrl)
+                        .send(demoAppClientData)
+                        .expect(200)
+                        .end(function(err, response) {
+                            expect(response).to.be.an('object');
+                            expect(response.body.data.accessToken).to.exist;
+                            demoAppClientToken = response.body.data.accessToken;
+                            done(err);
+                        });
+
+                }, 2000);
+
+            });
 
         });
 
-        describe('User login', function() {
+
+        describe('user login phrase', function() {
 
             var phraseUserLoginLocation;
             var userLoginPhrase = require('../../fixtures/phrases/phraseLoginUser.json');
 
-            it('creates the user login phrase', function(done) {
-              this.timeout(30000);
-              request(app)
-                  .put('/phrase')
-                  .set('Authorization', clientToken)
-                  .send(userLoginPhrase)
-                  .expect(204)
-                  .end(function(err, response) {
-                      expect(response.headers).to.exist;
-                      phraseUserLoginLocation = response.headers['location'];
-                      expect(phraseUserLoginLocation.length).to.be.above(0);
-                      done(err);
-                  });
+            it('is created correctly', function(done) {
+                this.timeout(30000);
+                request(app)
+                    .put('/phrase')
+                    .set('Authorization', clientToken)
+                    .send(userLoginPhrase)
+                    .expect(204)
+                    .end(function(err, response) {
+                        expect(response.headers).to.exist;
+                        phraseUserLoginLocation = response.headers['location'];
+                        expect(phraseUserLoginLocation.length).to.be.above(0);
+                        done(err);
+                    });
             });
 
-            it('calls the user login phrase and returns a user object, and access, expire and refresh tokens', function(done) {
+            it('receives a token/expires/refresh after calling it', function(done) {
                 var phraseEndpoint = userLoginPhrase.url;
                 var domain = phraseUserLoginLocation.replace('phrase/', '').split('!')[0];
                 clientLoginPhraseUrl = '/' + domain + '/' + phraseEndpoint;
@@ -116,6 +125,7 @@ function test(app) {
                             expect(response.body.tokenObject.expiresAt).to.exist;
                             expect(response.body.tokenObject.refreshToken).to.exist;
                             demoAppClientToken = response.body.tokenObject.accessToken;
+                            demoAppRefreshToken = response.body.tokenObject.refreshToken;
                             done(err);
                         });
 
@@ -123,6 +133,66 @@ function test(app) {
 
             });
 
+        });
+
+        describe('user tokenRefresh phrase', function() {
+
+            var refreshTokenLocation;
+            var refreshTokenPhrase = require('../../fixtures/phrases/refreshToken.json');
+
+            it('is created correctly', function(done) {
+                this.timeout(30000);
+                request(app)
+                    .put('/phrase')
+                    .set('Authorization', clientToken)
+                    .send(refreshTokenPhrase)
+                    .expect(204)
+                    .end(function(err, response) {
+                        expect(response.headers).to.exist;
+                        refreshTokenLocation = response.headers['location'];
+                        expect(refreshTokenLocation.length).to.be.above(0);
+                        done(err);
+                    });
+            });
+
+            it('can refresh user token with refreshToken', function(done) {
+                var phraseEndpoint = refreshTokenPhrase.url;
+                var domain = refreshTokenLocation.replace('phrase/', '').split('!')[0];
+                clientLoginPhraseUrl = '/' + domain + '/' + phraseEndpoint;
+
+                //Returns the data needed to make a user login
+                var data = {
+                    refreshToken: demoAppRefreshToken,
+                    scopes: clientUtils.getUser().scopes
+                };
+
+                this.timeout(30000);
+
+                //let's wait till corbel triggers the event to register the phrase in composr
+                //TODO: use any tool to know when it happens
+                setTimeout(function() {
+
+                    request(app)
+                        .post(clientLoginPhraseUrl)
+                        .set('Authorization', clientToken)
+                        .send(data)
+                        .expect(200)
+                        .end(function(err, response) {
+                            expect(response).to.be.an('object');
+                            expect(response.body).to.be.an('object');
+                            expect(response.body.data).to.be.an('object');
+                            expect(response.body.data.accessToken).to.exist;
+                            expect(response.body.data.expiresAt).to.exist;
+                            expect(response.body.data.refreshToken).to.exist;
+                            expect(response.body.data.refreshToken).to.not.be.equal(demoAppRefreshToken);
+                            demoAppClientToken = response.body.data.accessToken;
+                            demoAppRefreshToken = response.body.data.refreshToken;
+                            done(err);
+                        });
+
+                }, 2000);
+
+            });
         });
 
 
