28ddbc2a891728b376591ea1c408b5a95c33be69
Rafael Pedrola <rafinskipg@gmail.com>
2015-05-26 10:17:44 +0200

    added composr config environment variable
    Change-Id: I107beda2d08e9e5238ac613e4c0bc7fda0e5f550

--- src/config/config.js
+++ src/config/config.js
@@ -5,17 +5,28 @@ var env = process.env.NODE_ENV || 'development';
 var config = require('./config.json');
 
 
-//Check if enviroment config file exists and overwrite the defaults
+//Check if environment config file exists and overwrite the defaults
 var environmentFileConfig = env + '.json';
 
 try  {
 	var fstat = fs.statSync(__dirname + '/' + environmentFileConfig);
 	if (fstat.isFile()) {
-	    var envConfig = require('./' + environmentFileConfig);
-	    config = _.defaults(envConfig, config);
+	    var envConfigFile = require('./' + environmentFileConfig);
+	    config = _.defaults(envConfigFile, config);
 	}
 } catch (e) {
 	console.log('warn:config:' + env + ':undefined');
+}
+
+//Finally if it exists on the environment, use this
+if(process.env.COMPOSR_CONFIG){
+  try{
+    var envConfig = typeof(process.env.COMPOSR_CONFIG) === 'object' ? process.env.COMPOSR_CONFIG : JSON.parse(process.env.COMPOSR_CONFIG);;
+    config = _.defaults(envConfig, config);
+  }catch(e){
+    console.log(e);
+    console.log('warn:config:badformatedConfig' + env + ':undefined');
+  }
 }
 
 module.exports = config;
