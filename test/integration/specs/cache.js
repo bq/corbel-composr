3a603970dba75ba16fc19fcdb401424a385880a4
Anthanh <anthanh.pham@bq.com>
2015-05-27 09:16:53 +0200

    loginclient phrase fix for missin mandatory fields cache test added
    Change-Id: Ifcef50c4371ee45db6d75105acb1f6698e80481a

---
+++ test/integration/specs/cache.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var request = require('supertest'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+function test(app){
+  describe('Cache:', function() {
+
+      it('works', function(done) {
+
+          request(app)
+            .get('/cache')
+            .set('Authorization', 'sample_token')
+            .expect(200)
+            .end(function(error, response) {
+              expect(response).to.be.an('object');
+              if (!error && response.statusCode === 200) {
+                  request(app)
+                    .get('/cache')
+                    .set('Authorization', 'other_token')
+                    .set('Cache-Control', 'max-age=0')
+                    .set('If-None-Match', 'W/"d-9dc66274"')
+                    .expect(304)
+                    .end(function(error, response) {
+                      expect(response).to.be.an('object');
+                      if (!error && response.statusCode === 304) {
+                          return done();
+                      } else {
+                          return done(error || response);
+                      }
+                  });
+              } else {
+                  return done(error || response);
+              }
+          });
+
+      });
+
+  });
+
+}
+
+module.exports = test;
--- test/integration/test-suite.js
+++ test/integration/test-suite.js
@@ -4,10 +4,12 @@
 var timeoutTests = require('./specs/timeout.js'),
     errorHandlerTests = require('./specs/errorHandlers.js'),
     exampleTests = require('./specs/example.js'),
+    cacheTests = require('./specs/cache.js'),
     loginTests = require('./specs/login.js'),
     phraseTests = require('./specs/phrase.js');
 
 module.exports = function(app){
+  cacheTests(app);
   timeoutTests(app);
   errorHandlerTests(app);
   exampleTests(app);
