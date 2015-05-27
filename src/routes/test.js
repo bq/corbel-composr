43497a097b11f311dff67689f961c8f8d42598c8
Anthanh <anthanh.pham@bq.com>
2015-05-27 11:05:52 +0200

    cache route added
    Change-Id: I3818f571ace911f8ca9f897f1ffae29443bce68b

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,54 +1,54 @@
 'use strict';
 
 var express = require('express'),
-    router = express.Router(),
-    ComposerError = require('../lib/composerError'),
-    engine = require('../lib/engine'),
-    phraseManager = require('../lib/phraseManager');
+  router = express.Router(),
+  ComposerError = require('../lib/composerError'),
+  engine = require('../lib/engine'),
+  phraseManager = require('../lib/phraseManager');
 
 router.get('/e1', function(res) {
-    res.undefinedFunction();
+  res.undefinedFunction();
 });
 
 router.get('/e2', function() {
-    throw new ComposerError('error:custom', '', 555);
+  throw new ComposerError('error:custom', '', 555);
 });
 
 router.get('/t1', function(req, res) {
   setTimeout(function() {
-      res.send('Esto no debería verse');
+    res.send('Esto no debería verse');
   }, 100000);
 });
 
 router.get('/t2', function(req, res) {
 
-    var domain = require('domain').create();
+  var domain = require('domain').create();
 
-    domain.on('error', function(err) {
-      console.log('domain:error', err.message);
-    });
+  domain.on('error', function(err) {
+    console.log('domain:error', err.message);
+  });
 
-    domain.run(function() {
+  domain.run(function() {
 
-        var tripwire = require('tripwire');
+    var tripwire = require('tripwire');
 
-        tripwire.resetTripwire(1000);
+    tripwire.resetTripwire(1000);
 
-        var start = new Date();
-        var loops = 30000000000;
-        for (var i = 0; i < loops; i++) {
-            //process.nextTick();
-            //console.log(i / loops);
-        }
+    var start = new Date();
+    var loops = 30000000000;
+    for (var i = 0; i < loops; i++) {
+      //process.nextTick();
+      //console.log(i / loops);
+    }
 
-        var end = new Date();
-        console.log('delay', end.valueOf() - start.valueOf());
+    var end = new Date();
+    console.log('delay', end.valueOf() - start.valueOf());
 
-        res.send('Esto no debería verse');
+    res.send('Esto no debería verse');
 
-        tripwire.clearTripwire();
+    tripwire.clearTripwire();
 
-    });
+  });
 
 });
 
@@ -56,15 +56,15 @@ router.get('/t2', function(req, res) {
 router.get('/t2phrase', function(req, res, next) {
 
 
-  var phrase = function phrase(){
+  var phrase = function phrase() {
     //Code of the phrase
     console.log('start bad phrase');
 
     var start = new Date();
     var loops = 30000000000;
     for (var i = 0; i < loops; i++) {
-        //process.nextTick();
-        //console.log(i / loops);
+      //process.nextTick();
+      //console.log(i / loops);
     }
 
     var end = new Date();
@@ -81,15 +81,15 @@ router.get('/t2phrase', function(req, res, next) {
     res: res,
     next: next
   };
-  phraseManager.executePhrase(context,null, body);
+  phraseManager.executePhrase(context, null, body);
 
 });
 
 router.get('/t3phrase', function(req, res) {
 
-  var phrase = function phrase(){
+  var phrase = function phrase() {
     res.json({
-      yes : 'potatoe'
+      yes: 'potatoe'
     });
   };
 
@@ -101,7 +101,7 @@ router.get('/t3phrase', function(req, res) {
     res: res
   };
 
-  phraseManager.executePhrase(context,null,body);
+  phraseManager.executePhrase(context, null, body);
 });
 
 router.get('/t4snippet', function(req, res) {
@@ -116,5 +116,12 @@ router.get('/t4snippet', function(req, res) {
   phraseManager.executePhrase(context, compoSR, phraseBody);
 });
 
+
+router.get('/cache', function(req, res) {
+  res.set('Cache-Control', 'public, max-age=31536000');
+  res.json({
+    data: true
+  });
+});
 
 module.exports = router;
