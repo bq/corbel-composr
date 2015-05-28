7f7994cf1ca20aca982a745933c28d2ef2023937
Anthanh <anthanh.pham@bq.com>
2015-05-28 12:19:48 +0200

    code format
    Change-Id: Ic451bad9e92935295eb1525b9a948ca7b6c46822

--- src/lib/worker.js
+++ src/lib/worker.js
@@ -1,96 +1,94 @@
 'use strict';
 
 var phraseManager = require('./phraseManager'),
-    connection = require('./corbelConnection'),
-    express = require('express'),
-    router = express.Router(),
-    amqp = require('amqplib'),
-    uuid = require('uuid'),
-    ComposerError = require('./composerError'),
-    config = require('./config'),
-    logger = require('../utils/logger');
+  connection = require('./corbelConnection'),
+  express = require('express'),
+  router = express.Router(),
+  amqp = require('amqplib'),
+  uuid = require('uuid'),
+  ComposerError = require('./composerError'),
+  config = require('./config'),
+  logger = require('../utils/logger');
 
 var worker = function() {
-    var connUrl = 'amqp://' + config('rabbitmq.username') + ':' + config('rabbitmq.password') + '@' + config('rabbitmq.host') + ':' + config('rabbitmq.port');
+  var connUrl = 'amqp://' + config('rabbitmq.username') + ':' + config('rabbitmq.password') + '@' + config('rabbitmq.host') + ':' + config('rabbitmq.port');
 
-    amqp.connect(connUrl).then(function(conn) {
+  amqp.connect(connUrl).then(function(conn) {
 
-        function doWork(msg) {
-            if (msg.fields.routingKey === config('rabbitmq.event')) {
-                logger.debug('ROUTINGKEY', msg.content.toString());
+    function doWork(msg) {
+      if (msg.fields.routingKey === config('rabbitmq.event')) {
+        logger.debug('ROUTINGKEY', msg.content.toString());
 
-                var message;
-                try {
-                    message = JSON.parse(msg.content.toString());
-                } catch (error) {
-                    throw new ComposerError('error:worker:message', 'Error parsing message: ' + error, 422);
-                }
-
-                logger.debug('TYPE', message.type);
-                if (message.type === connection.PHRASES_COLLECTION) {
-                    logger.debug('PHRASES_COLLECTION', message);
-                    switch (message.action) {
-                        case 'DELETE':
-
-                            phraseManager.unregisterPhrase(router, {
-                                id: message.resourceId
-                            });
-                            break;
-
-                        default: // 'CREATE' or 'UPDATE'
-
-                            connection.driver.then(function(driver) {
-                                return driver.resources.resource(connection.PHRASES_COLLECTION, message.resourceId).get();
-                            }).then(function(response) {
-                                phraseManager.registerPhrase(router, response.data);
-                            });
-                            break;
-
-                    }
-
-                }
-            }
+        var message;
+        try {
+          message = JSON.parse(msg.content.toString());
+        } catch (error) {
+          throw new ComposerError('error:worker:message', 'Error parsing message: ' + error, 422);
         }
 
-        return conn.createChannel().then(function(ch) {
-            process.once('SIGINT', function() {
-                conn.close();
-                process.exit();
-            });
+        logger.debug('TYPE', message.type);
+        if (message.type === connection.PHRASES_COLLECTION) {
+          logger.debug('PHRASES_COLLECTION', message);
+          switch (message.action) {
+            case 'DELETE':
 
-            var queue = 'composer-' + uuid.v4(),
-                exchange = 'eventbus.exchange',
-                pattern = '';
+              phraseManager.unregisterPhrase(router, {
+                id: message.resourceId
+              });
+              break;
 
-            var ok = ch.assertQueue(queue, {
-                durable: false,
-                autoDelete: true
-            });
+            default: // 'CREATE' or 'UPDATE'
 
-            ok = ok.then(function() {
-                ch.bindQueue(queue, exchange, pattern);
-            });
+              connection.driver.then(function(driver) {
+                return driver.resources.resource(connection.PHRASES_COLLECTION, message.resourceId).get();
+              }).then(function(response) {
+                phraseManager.registerPhrase(router, response.data);
+              });
+              break;
 
-            ok = ok.then(function() {
-                ch.consume(queue, doWork, {
-                    noAck: true
-                });
-                logger.debug('Worker up');
-            });
+          }
 
-            return ok;
+        }
+      }
+    }
+
+    return conn.createChannel().then(function(ch) {
+      process.once('SIGINT', function() {
+        conn.close();
+        process.exit();
+      });
+
+      var queue = 'composer-' + uuid.v4(),
+        exchange = 'eventbus.exchange',
+        pattern = '';
+
+      var ok = ch.assertQueue(queue, {
+        durable: false,
+        autoDelete: true
+      });
+
+      ok = ok.then(function() {
+        ch.bindQueue(queue, exchange, pattern);
+      });
+
+      ok = ok.then(function() {
+        ch.consume(queue, doWork, {
+          noAck: true
         });
-    }).then(null, function(err) {
-        logger.error('Worker error %s', err);
-        setTimeout(worker, config('rabbitmq.reconntimeout'));
+        logger.debug('Worker up');
+      });
+
+      return ok;
     });
+  }).then(null, function(err) {
+    logger.error('Worker error %s', err);
+    setTimeout(worker, config('rabbitmq.reconntimeout'));
+  });
 };
 
-
-
 module.exports = {
-  router : router,
-  init : function(){
+  router: router,
+  init: function() {
     worker();
   }
 };
