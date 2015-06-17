49095b8738891d4d61dcd3249244bd9b9e1d0863
Rafael Pedrola <rafinskipg@gmail.com>
2015-06-17 12:27:22 +0200

    chore(jsdoc): Adding jsdoc build task
    Change-Id: I2e85ebf14fc2e458036e8f48d2af9e3f2e3c954e

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,152 +3,174 @@
 var request = require('request');
 var madge = require('madge');
 
+
 module.exports = function(grunt) {
-    // show elapsed time at the end
-    require('time-grunt')(grunt);
-    // load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-    var reloadPort = 35729,
-        files;
+  var reloadPort = 35729,
+    files;
 
-    grunt.initConfig({
+  function jsdocTask() {
+    var exec = require('child_process').exec;
+    /* jshint validthis:true */
+    var done = this.async();
 
-        pkg: grunt.file.readJSON('./package.json'),
+    var jsdocCmd = './node_modules/jsdoc/jsdoc';
 
-        clean: {
-            all: ['.tmp']
+    var src = ' -r src';
+    var dest = ' -d doc/jsdoc';
+    var config = ' -c .jsdoc';
+    var template = ' -t node_modules/jaguarjs-jsdoc';
+
+    exec(jsdocCmd + src + dest + config + template, function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      done(err === null);
+    });
+  }
+
+  grunt.registerTask('jsdoc', 'Generates JSDoc', jsdocTask);
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('./package.json'),
+
+    clean: {
+      all: ['.tmp']
+    },
+
+    copy: {
+      coverage: {
+        expand: true,
+        src: [
+          'test/**',
+          'src/**',
+          'bin/**',
+          // express files
+          'public/**',
+          'package.json'
+        ],
+        dest: '.tmp/coverage/'
+      }
+    },
+
+    blanket: {
+      coverage: {
+        src: ['.tmp/coverage/src/'],
+        dest: '.tmp/coverage/src/'
+      }
+    },
+
+    develop: {
+      server: {
+        file: 'bin/composer'
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'src/**/*.js'
+      ]
+    },
+
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      server: {
+        files: [
+          'bin/composer',
+          'src/**/*.js'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      'public': {
+        files: [
+          'public/**/*.js',
+          'public/**/*.css',
+          'public/**/*.html',
+          'src/**/*.ejs'
+        ],
+        options: {
+          livereload: reloadPort
+        }
+      },
+      test: {
+        files: ['test/**/*.js', 'src/lib/**/*.js'],
+        tasks: ['mochaTest:unit']
+      }
+    },
+
+    mochaTest: { //test for nodejs app with mocha
+      testCoverage: {
+        options: {
+          reporter: 'spec',
         },
-
-        copy: {
-            coverage: {
-                expand: true,
-                src: [
-                    'test/**',
-                    'src/**',
-                    'bin/**',
-                    // express files
-                    'public/**',
-                    'package.json'
-                ],
-                dest: '.tmp/coverage/'
-            }
+        src: ['.tmp/coverage/test/runner.js']
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          quiet: true,
+          captureFile: '.tmp/coverage/coverage.html'
         },
-
-        blanket: {
-            coverage: {
-                src: ['.tmp/coverage/src/'],
-                dest: '.tmp/coverage/src/'
-            }
+        src: ['.tmp/coverage/test/runner.js']
+      },
+      coveralls: {
+        options: {
+          reporter: 'mocha-lcov-reporter',
+          quiet: true,
+          captureFile: '.tmp/coverage/lcov.info'
         },
-
-        develop: {
-            server: {
-                file: 'bin/composer'
-            }
+        src: ['.tmp/coverage/test/runner.js']
+      },
+      'travis-cov': {
+        options: {
+          reporter: 'travis-cov'
         },
-
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'src/**/*.js'
-            ]
+        src: ['.tmp/coverage/test/runner.js']
+      },
+      tap: {
+        options: {
+          reporter: 'tap',
+          captureFile: 'target/test_results.dirty.tap', // Optionally capture the reporter output to a file
+          quiet: false // Optionally suppress output to standard out (defaults to false)
         },
-
-        watch: {
-            options: {
-                nospawn: true,
-                livereload: reloadPort
-            },
-            server: {
-                files: [
-                    'bin/composer',
-                    'src/**/*.js'
-                ],
-                tasks: ['develop', 'delayed-livereload']
-            },
-            'public': {
-                files: [
-                    'public/**/*.js',
-                    'public/**/*.css',
-                    'public/**/*.html',
-                    'src/**/*.ejs'
-                ],
-                options: {
-                    livereload: reloadPort
-                }
-            },
-            test : {
-              files: ['test/**/*.js', 'src/lib/**/*.js'],
-              tasks: ['mochaTest:unit']
-            }
+        src: ['test/runner.js']
+      },
+      ci: {
+        options: {
+          reporter: 'spec'
         },
-
-        mochaTest: { //test for nodejs app with mocha
-            testCoverage: {
-                options: {
-                    reporter: 'spec',
-                },
-                src: ['.tmp/coverage/test/runner.js']
-            },
-            coverage: {
-                options: {
-                    reporter: 'html-cov',
-                    quiet: true,
-                    captureFile: '.tmp/coverage/coverage.html'
-                },
-                src: ['.tmp/coverage/test/runner.js']
-            },
-            coveralls: {
-                options: {
-                    reporter: 'mocha-lcov-reporter',
-                    quiet: true,
-                    captureFile: '.tmp/coverage/lcov.info'
-                },
-                src: ['.tmp/coverage/test/runner.js']
-            },
-            'travis-cov': {
-                options: {
-                    reporter: 'travis-cov'
-                },
-                src: ['.tmp/coverage/test/runner.js']
-            },
-            tap: {
-                options: {
-                    reporter: 'tap',
-                    captureFile: 'target/test_results.dirty.tap', // Optionally capture the reporter output to a file
-                    quiet: false // Optionally suppress output to standard out (defaults to false)
-                },
-                src: ['test/runner.js']
-            },
-            ci: {
-                options: {
-                    reporter: 'spec'
-                },
-                src: ['test/runner.js']
-            },
-            unit: {
-                options: {
-                    reporter: 'spec'
-                },
-                src: ['test/unit/test-suite.js']
-            }
+        src: ['test/runner.js']
+      },
+      unit: {
+        options: {
+          reporter: 'spec'
         },
+        src: ['test/unit/test-suite.js']
+      }
+    },
 
-        express: {
-            composer: {
-                options: {
-                    script: 'bin/composer',
-                    logs: {
-                        out: 'composer.out.log',
-                        err: 'composer.err.log'
-                    }
-                }
-            },
-            /*coverage: {
+    express: {
+      composer: {
+        options: {
+          script: 'bin/composer',
+          logs: {
+            out: 'composer.out.log',
+            err: 'composer.err.log'
+          }
+        }
+      },
+      /*coverage: {
                 options: {
                     script: 'bin/composer.coverage',
                     logs: {
@@ -157,91 +179,91 @@ module.exports = function(grunt) {
                     }
                 }
             }*/
-        },
+    },
 
-        coveralls: {
-            options: {
-                force: false
-            },
-            'default': {
-                src: '.tmp/coverage/lcov.info'
-            }
-        },
-
-        release: {
-            /* For more options: https://github.com/geddski/grunt-release#options */
-            options: {
-                indentation: '\t', //default: '  ' (two spaces)
-                commitMessage: 'Release v<%= version %>', //default: 'release <%= version %>'
-                tagMessage: 'v<%= version %>', //default: 'Version <%= version %>',
-                tagName: 'v<%= version %>'
-            }
-        }
-
-    });
-
-    grunt.config.requires('watch.server.files');
-    files = grunt.config('watch.server.files');
-    files = grunt.file.expand(files);
-
-    //Register circular dependencies
-    grunt.registerTask('madge', 'Run madge.', function() {
-      var dependencyObject = madge('./src');
-      var listOfCircularDependencies = dependencyObject.circular().getArray();
-
-      if(listOfCircularDependencies.length > 0){
-        grunt.log.error('CIRCULAR DEPENDENCIES FOUND');
-        grunt.log.error(listOfCircularDependencies);
-        return false;
-      }else{
-        grunt.log.writeln('No circular dependencies found :)');
+    coveralls: {
+      options: {
+        force: false
+      },
+      'default': {
+        src: '.tmp/coverage/lcov.info'
       }
-    });
+    },
+
+    release: {
+      /* For more options: https://github.com/geddski/grunt-release#options */
+      options: {
+        indentation: '\t', //default: '  ' (two spaces)
+        commitMessage: 'Release v<%= version %>', //default: 'release <%= version %>'
+        tagMessage: 'v<%= version %>', //default: 'Version <%= version %>',
+        tagName: 'v<%= version %>'
+      }
+    }
+
+  });
+
+  grunt.config.requires('watch.server.files');
+  files = grunt.config('watch.server.files');
+  files = grunt.file.expand(files);
+
+  //Register circular dependencies
+  grunt.registerTask('madge', 'Run madge.', function() {
+    var dependencyObject = madge('./src');
+    var listOfCircularDependencies = dependencyObject.circular().getArray();
+
+    if (listOfCircularDependencies.length > 0) {
+      grunt.log.error('CIRCULAR DEPENDENCIES FOUND');
+      grunt.log.error(listOfCircularDependencies);
+      return false;
+    } else {
+      grunt.log.writeln('No circular dependencies found :)');
+    }
+  });
 
 
-    grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function() {
-        var done = this.async();
-        setTimeout(function() {
-            request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','), function(err, res) {
-                var reloaded = !err && res.statusCode === 200;
-                if (reloaded) {
-                    grunt.log.ok('Delayed live reload successful.');
-                } else {
-                    grunt.log.error('Unable to make a delayed live reload.');
-                }
-                done(reloaded);
-            });
-        }, 500);
-    });
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function() {
+    var done = this.async();
+    setTimeout(function() {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','), function(err, res) {
+        var reloaded = !err && res.statusCode === 200;
+        if (reloaded) {
+          grunt.log.ok('Delayed live reload successful.');
+        } else {
+          grunt.log.error('Unable to make a delayed live reload.');
+        }
+        done(reloaded);
+      });
+    }, 500);
+  });
 
-    grunt.registerTask('default', [
-        'clean',
-        'jshint',
-        'test',
-        'develop',
-        'watch'
-    ]);
+  grunt.registerTask('default', [
+    'clean',
+    'jshint',
+    'test',
+    'develop',
+    'watch'
+  ]);
 
-    grunt.registerTask('test:coverage', [
-        'clean',
-        'jshint',
-        'madge',
-        'copy:coverage',
-        'blanket',
-        'mochaTest:testCoverage',
-        'mochaTest:coverage',
-        'mochaTest:coveralls',
-        'mochaTest:travis-cov',
-        'coveralls'
-    ]);
+  grunt.registerTask('test:coverage', [
+    'clean',
+    'jshint',
+    'madge',
+    'copy:coverage',
+    'blanket',
+    'mochaTest:testCoverage',
+    'mochaTest:coverage',
+    'mochaTest:coveralls',
+    'mochaTest:travis-cov',
+    'coveralls'
+  ]);
 
-    grunt.registerTask('test', [
-        'jshint',
-        'madge',
-        'mochaTest:ci'
-    ]);
+  grunt.registerTask('test', [
+    'jshint',
+    'madge',
+    'mochaTest:ci'
+  ]);
 
-    grunt.registerTask('unit', [
-        'mochaTest:unit'
-    ]);
+  grunt.registerTask('unit', [
+    'mochaTest:unit'
+  ]);
 };
