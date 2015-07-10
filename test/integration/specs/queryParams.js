e51a68ee9a6d1ab8aa497632c1442be958b8b018
Rafael Pedrola <rafinskipg@gmail.com>
2015-07-10 12:13:56 +0200

    ensure the query params are received correctly
    Change-Id: Ia2b07efeda36caff0e687769f44dd571c059e5a0

--- test/integration/specs/queryParams.js
+++ test/integration/specs/queryParams.js
@@ -13,35 +13,33 @@ function test(app) {
       var domain = 'test';
       var stub;
 
-      var phrases = [
-        {
-          url: ':param',
-          callee: 'hola?user=pepe',
-          test: {
-            user: 'pepe'
-          }
-        },{
-          url: ':param?',
-          callee: 'asd?casa=roja&moto=verde',
-          test: {
-            casa: 'roja',
-            moto : 'verde'
-          }
-        }, {
-          url: 'logoutuser/:type?',
-          callee: 'logoutuser?casa=roja&moto=verde',
-          test: {
-            casa: 'roja',
-            moto : 'verde'
-          }
-        }, {
-          url: 'pepito',
-          callee: 'pepito?hola=mundo',
-          test: {
-            hola : 'mundo'
-          }
+      var phrases = [{
+        url: ':param',
+        callee: 'hola?user=pepe',
+        test: {
+          user: 'pepe'
         }
-      ];
+      }, {
+        url: ':param?',
+        callee: 'asd?casa=roja&moto=verde',
+        test: {
+          casa: 'roja',
+          moto: 'verde'
+        }
+      }, {
+        url: 'logoutuser/:type?',
+        callee: 'logoutuser?casa=roja&moto=verde',
+        test: {
+          casa: 'roja',
+          moto: 'verde'
+        }
+      }, {
+        url: 'pepito',
+        callee: 'pepito?hola=mundo',
+        test: {
+          hola: 'mundo'
+        }
+      }];
 
       var PHRASE_INDEX = 0;
 
@@ -56,7 +54,7 @@ function test(app) {
         });
       });
 
-      after(function(){
+      after(function() {
         stub.restore();
       });
 
@@ -71,9 +69,78 @@ function test(app) {
           .get('/mydomain/' + phrase.callee)
           .expect(200)
           .end(function(err, response) {
-            
+
             Object.keys(phrase.test).forEach(function(key) {
               expect(phrase.test[key]).to.equals(response.body[key]);
+            })
+
+            if (PHRASE_INDEX < phrases.length - 1) {
+              PHRASE_INDEX++
+              stub.restore();
+              executeTest(cb);
+            } else {
+              cb();
+            }
+          })
+
+      }
+
+      it('does receive all the query params as expected', function(done) {
+        this.timeout(30000);
+        executeTest(done);
+      });
+
+    });
+
+
+    describe('corbel-js search query params', function() {
+      var domain = 'test';
+      var stub;
+
+      var phrases = [{
+        url: ':moto',
+        callee: 'moto?query=[{\"$eq\":{\"titleText\": \"Praga De cerca 3\"}}]',
+        test: {
+          query: {
+            type: 'string',
+            value: "[{'$eq': { 'titleText': 'Praga De cerca 3' } }]"
+          }
+        }
+      }];
+
+      var PHRASE_INDEX = 0;
+
+      before(function() {
+        phrases = phrases.map(function(phrase) {
+          phrase.get = {
+            code: 'res.send(req.query);'
+          };
+          phrase.regexpReference = regexpGenerator.regexpReference(phrase.url);
+          phraseManager.cacheMethods(phrase);
+          return phrase;
+        });
+      });
+
+      after(function() {
+        stub.restore();
+      });
+
+      function executeTest(cb) {
+        var phrase = phrases[PHRASE_INDEX];
+
+        stub = sinon.stub(phraseManager, 'getPhraseByMatchingPath', function() {
+          return this;
+        }.bind(phrase));
+
+        request(app)
+          .get('/mydomain/' + phrase.callee)
+          .expect(200)
+          .end(function(err, response) { 
+            console.log(response.body);
+
+            Object.keys(phrase.test).forEach(function(key) {
+              var typeofParam = typeof(response.body[key]);
+              expect(phrase.test[key].type).to.equals(typeofParam);
             })
 
             if (PHRASE_INDEX < phrases.length - 1) {
