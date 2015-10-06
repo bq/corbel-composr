fe65556297a7cf2bbe0ce253fe0d7e7212245cd3
Rafael Pedrola <rafinskipg@gmail.com>
2015-10-06 16:02:52 +0200

    started refactoring worker
    Change-Id: I19d97e3d583ed7fac757a3f8d6c9a471852201ea

--- src/lib/engine.js
+++ src/lib/engine.js
@@ -2,10 +2,13 @@
 
 var logger = require('../utils/logger'),
   composr = require('composr-core'),
+  q = require('q'),
+  https = require('https'),
   config = require('./config');
 
 var phrasesCollection = 'composr:Phrase';
 var snippetsCollection = 'composr:Snippet';
+var initialized = false;
 
 function suscribeLogger() {
   composr.events.on('debug', 'CorbelComposr', function() {
@@ -26,12 +29,54 @@ function suscribeLogger() {
   });
 }
 
+
+
+function waitTilServicesUp(cb, time, retries) {
+  var modules = ['iam', 'resources'];
+  var path = config('corbel.driver.options').urlBase;
+
+  if (retries <= 0) {
+    cb(true);
+  } else {
+    var promises = modules.map(function(module) {
+      logger.info('Checking for external service', module);
+      var deferred = q.defer();
+
+      https.get(path.replace('{{module}}', module) + '/version', function() {
+        deferred.resolve();
+        logger.info('External service', module, 'is UP');
+      })
+        .on('error', function() {
+          logger.error('External service', module, 'is DOWN');
+          deferred.reject();
+        });
+
+      return deferred.promise;
+    });
+
+    q.all(promises)
+      .then(function() {
+        logger.info('All Services up and running!');
+        cb();
+      })
+      .catch(function() {
+        logger.info('Retrying services check after', time * retries, 'milliseconds');
+        setTimeout(function() {
+          waitTilServicesUp(cb, time, retries - 1);
+        }, time * retries);
+      });
+  }
+
+}
+
+
 /**
  * Launches the bootstrap of data, worker and logger
  * @param  {[type]} app [description]
  * @return promise
  */
 function init(app) {
+  var dfd = q.defer();
 
   suscribeLogger();
 
@@ -43,21 +88,43 @@ function init(app) {
     },
     urlBase: config('corbel.driver.options').urlBase
   };
+  var retries = 30;
 
-  return composr.init(coreOptions)
-    .then(function() {
-      logger.info('Engine initialized, all data is loaded :)');
-      return {
-        app: app,
-        composr : composr
-      };
-    });
+  waitTilServicesUp(function(err) {
 
+    if (err) {
+      logger.error('Services where unaccesible after ' + retries + ' retries');
+      dfd.reject('error:accessing:services');
+    } else {
+      composr.init(coreOptions)
+        .then(function() {
+          initialized = true;
+          logger.info('Engine initialized, all data is loaded :)');
+          dfd.resolve({
+            app: app,
+            composr: composr,
+            initialized: initialized
+          });
+        })
+        .catch(function(err) {
+          initialized = false;
+          logger.error('ERROR launching composr, please check your credentials and network');
+          logger.error(err);
+          dfd.reject(err);
+          //TODO: notify healthcheck services
+        });
+    }
+
+  }, 1000, retries);
+
+  return dfd.promise;
 }
+
 
 module.exports = {
   init: init,
-  composr : composr,
-  phrasesCollection : phrasesCollection,
-  snippetsCollection : snippetsCollection
+  composr: composr,
+  initialized: initialized,
+  phrasesCollection: phrasesCollection,
+  snippetsCollection: snippetsCollection
 };
