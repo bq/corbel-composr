f2502a85b5be1a24e7db433d12930a3d5bd1b8ed
Rafael Pedrola <rafinskipg@gmail.com>
2015-10-06 16:02:52 +0200

    started refactoring worker
    Change-Id: I19d97e3d583ed7fac757a3f8d6c9a471852201ea

--- src/lib/worker.js
+++ src/lib/worker.js
@@ -1,127 +1,121 @@
 'use strict';
 
 var engine = require('./engine'),
-  connection = require('./corbelConnection'),
+  corbelConnection = require('./corbelConnection'),
   amqp = require('amqplib'),
   uuid = require('uuid'),
-  q = require('q'),
   ComposrError = require('./ComposrError'),
   config = require('./config'),
   logger = require('../utils/logger');
 
-var worker = function() {
-  var dfd = q.defer();
+function doWork(ch, msg) {
+  if (msg.fields.routingKey === config('rabbitmq.event')) {
 
-  var connUrl = 'amqp://' + config('rabbitmq.username') + ':' + config('rabbitmq.password') + '@' + config('rabbitmq.host') + ':' + config('rabbitmq.port');
-
-  var id = Date.now();
-
-  logger.debug('WORKER', 'created worker with id', id);
-
-  amqp.connect(connUrl).then(function(conn) {
-    logger.debug('WORKER Connected: ', id);
-
-    function doWork(ch, msg) {
-      if (msg.fields.routingKey === config('rabbitmq.event')) {
-
-        var message;
-        try {
-          message = JSON.parse(msg.content.toString('utf8'));
-        } catch (error) {
-          //ch.nack(error, false, false);
-          throw new ComposrError('error:worker:message', 'Error parsing message: ' + error, 422);
-        }
-
-        if (message.type === connection.PHRASES_COLLECTION) {
-          logger.debug('WORKER mesage', message);
-          switch (message.action) {
-            case 'DELETE':
-              var id = message.resourceId;
-              var domain = id.split('!')[0];
-              engine.composr.Phrases.unregister(domain, id);
-              
-              //ch.ack(msg);
-              break;
-
-            default: // 'CREATE' or 'UPDATE'
-              logger.debug('WORKER triggered create or update event', message.resourceId);
-
-              connection.driver.then(function(driver) {
-                  return driver.resources.resource(connection.PHRASES_COLLECTION, message.resourceId).get();
-                }).then(function(response) {
-                  var domain = response.data.id.split('!', [0]);
-                  engine.composr.Phrases.register(domain, response.data);
-                  //ch.ack(msg);
-                })
-                .catch(function(err) {
-                  logger.error('WORKER error: ', err);
-                  connection.regenerateDriver();
-                  throw new ComposrError('error:worker:phrase', 'Error registering phrase: ' + err, 422);
-                  //ch.nack(err, false, false);
-                });
-
-              break;
-
-          }
-
-        }
-
-      }
+    var message;
+    try {
+      message = JSON.parse(msg.content.toString('utf8'));
+    } catch (error) {
+      //ch.nack(error, false, false);
+      throw new ComposrError('error:worker:message', 'Error parsing message: ' + error, 422);
     }
 
-    return conn.createChannel().then(function(ch) {
+    if (message.type === corbelConnection.PHRASES_COLLECTION) {
+      logger.debug('WORKER phrases event:', message);
+      switch (message.action) {
+        case 'DELETE':
+          var id = message.resourceId;
+          var domain = id.split('!')[0];
+          engine.composr.Phrases.unregister(domain, id);
+
+          //ch.ack(msg);
+          break;
+
+        default: // 'CREATE' or 'UPDATE'
+          var id = message.resourceId;
+          var domain = id.split('!')[0];
+          logger.debug('WORKER triggered create or update event', id);
+          engine.composr.loadPhrase(id)
+            .then(function(phrase){
+              console.log(phrase);
+              return engine.composr.Phrases.register(domain, phrase);
+            })
+            .then(function(result){
+              console.log(result);
+            })
+            .catch(function(err){
+              logger.error('WORKER error: ', err);
+            });
+        break;
+
+      }
+
+    }
+  }
+
+}
+
+function createChannel(conn) {
+  var queue = 'composer-' + uuid.v4(),
+    exchange = 'eventbus.exchange',
+    pattern = '';
+
+  return conn.createChannel()
+    .then(function(ch) {
+      return ch.assertQueue(queue, {
+          durable: false,
+          autoDelete: true
+        }).then(function() {
+          return ch.bindQueue(queue, exchange, pattern);
+        })
+        .then(function() {
+          ch.consume(queue, function(message) {
+              //Added callback function in case we need to do manual ack of the messages
+              doWork(ch, message);
+            },
+            Object.create({
+              noAck: true
+            }));
+        });
+    });
+}
+
+
+function init() {
+  var connUrl = 'amqp://' + config('rabbitmq.username') + ':' + config('rabbitmq.password') + '@' + config('rabbitmq.host') + ':' + config('rabbitmq.port');
+  var conn;
+
+  var workerID = uuid.v4();
+
+  logger.info('Creating worker with ID', workerID);
+
+  amqp.connect(connUrl)
+    .then(function(connection) {
+      conn = connection;
+      //Close conection on SIGINT
       process.once('SIGINT', function() {
-        conn.close();
+        connection.close();
         process.exit();
       });
 
-      var queue = 'composer-' + uuid.v4(),
-        exchange = 'eventbus.exchange',
-        pattern = '';
-
-      var ok = ch.assertQueue(queue, {
-        durable: false,
-        autoDelete: true
-      }).then(function() {
-
-        return ch.bindQueue(queue, exchange, pattern);
-
-      }).then(function() {
-
-        ch.consume(queue, function(message) {
-            //Added callback function in case we need to do manual ack of the messages
-            doWork(ch, message);
-          },
-          Object.create({
-            noAck: true
-          }));
-
-        logger.debug('Worker up');
-        dfd.resolve();
-      });
-
-      return ok;
-
-    }).catch(function(err) {
-
-      logger.error('WORKER: error creating channel', err);
-      dfd.reject(err);
-      if (conn) {
-        conn.close(function() {
-          process.exit(1);
+      createChannel(connection)
+        .then(function() {
+          logger.info('Worker up, with ID', workerID);
+        })
+        .catch(function(error) {
+          logger.error('WORKER error ', error, 'with ID', workerID);
+          if (conn) {
+            conn.close(function() {
+              process.exit(1);
+            });
+          }
         });
-      }
-
+    })
+    .then(null, function(err) {
+      logger.error('Worker error %s with ID : %s', err, workerID);
+      setTimeout(init, config('rabbitmq.reconntimeout'));
     });
-
-  }).then(null, function(err) {
-    logger.error('Worker error %s', err);
-    setTimeout(worker, config('rabbitmq.reconntimeout'));
-  });
-
-  return dfd.promise;
-};
+}
 
 module.exports = {
-  init: worker
+  init: init
 };
