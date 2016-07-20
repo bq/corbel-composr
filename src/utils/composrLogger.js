b5c8e3084c505b6e6a8030e82a677685d81b6f46
jarpi <josep.arpi@gmail.com>
2016-07-20 14:56:16 +0200

    Added date/time in logs

--- src/utils/composrLogger.js
+++ src/utils/composrLogger.js
@@ -6,15 +6,19 @@ var art = require('ascii-art')
 var config = require('config')
 var logLevel = config.get('composrLog.logLevel') ? config.get('composrLog.logLevel') : false
 var logFile = config.get('composrLog.logFile') ? config.get('composrLog.logFile') : false
+var moment = require('moment')
 
 var logger
 
 if (logLevel) {
   // Default transports
   var transports = [
-    new (winston.transports.Console)({
+    new(winston.transports.Console)({
       level: logLevel,
-      colorize: true
+      colorize: true,
+      timestamp: function() {
+        return moment().format('DD-MM-YYYY-HH:mm')
+      }
     })
   ]
 
@@ -23,15 +27,18 @@ if (logLevel) {
     var logDirs = folderMaker.getPathFromFilePath(logFile)
     folderMaker.makePath(logDirs)
 
-    transports.push(new (winston.transports.File)({
+    transports.push(new(winston.transports.File)({
       name: 'error-file',
       filename: logFile,
-      level: logLevel
+      level: logLevel,
+      timestamp: function() {
+        return moment().format('DD-MM-YYYY-HH:mm')
+      }
     }))
   }
 
   // Initialize logger
-  logger = new (winston.Logger)({
+  logger = new(winston.Logger)({
     transports: transports
   })
 }
@@ -40,25 +47,25 @@ if (logLevel) {
   Logger levels
   @todo: Prevent circular reference problem
 **/
-function error () {
+function error() {
   if (logLevel) {
     logger.log.apply(logger, ['error'].concat(Array.prototype.slice.call(arguments)))
   }
 }
 
-function info () {
+function info() {
   if (logLevel) {
     logger.log.apply(logger, ['info'].concat(Array.prototype.slice.call(arguments)))
   }
 }
 
-function warn () {
+function warn() {
   if (logLevel) {
     logger.log.apply(logger, ['warn'].concat(Array.prototype.slice.call(arguments)))
   }
 }
 
-function debug () {
+function debug() {
   if (logLevel) {
     try {
       logger.log.apply(logger, ['debug'].concat(Array.prototype.slice.call(arguments)))
@@ -68,15 +75,15 @@ function debug () {
   }
 }
 
-function fancy (text, cb) {
-  art.font(text, 'Basic', 'green', function (rendered) {
+function fancy(text, cb) {
+  art.font(text, 'Basic', 'green', function(rendered) {
     console.log(rendered)
     cb()
   })
 }
 
-function salute (serverName, version, cb) {
-  art.font(serverName, 'Doom', 'bright_green').font(version, 'Doom', 'bright_cyan', function (rendered) {
+function salute(serverName, version, cb) {
+  art.font(serverName, 'Doom', 'bright_green').font(version, 'Doom', 'bright_cyan', function(rendered) {
     console.log(rendered)
     cb()
   })
