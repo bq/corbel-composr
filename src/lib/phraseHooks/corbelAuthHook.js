fefeb90cac04f2cae0b23f635e916f3f1a38d1e1
Jorge  Serrano <jorge.serrano@bq.com>
2016-08-10 18:19:44 +0200

    added signing requests option

--- src/lib/phraseHooks/corbelAuthHook.js
+++ src/lib/phraseHooks/corbelAuthHook.js
@@ -5,7 +5,15 @@ var ComposrError = require('../ComposrError')
 var logger = require('../../utils/composrLogger')
 var config = require('config')
 var corbel = require('corbel-js')
+var redisConnector = require('../connectors/redis')
+var timeParser = require('parse-duration')
+// Days to seconds
+var _duration = config.get('signRequests.durationInDays')
+var _durationInSeconds = parseInt(_duration) * 60 * 60 * 24
 
+/**
+ * Auth user middleware
+ */
 module.exports.authUser = function () {
   return function authUser (req, res, next) {
     var authHeader = req.header('Authorization')
@@ -17,8 +25,17 @@ module.exports.authUser = function () {
     try {
       var jwtDecoded = corbel.jwt.decode(authHeader.replace('Bearer ', ''))
       var userId = jwtDecoded.userId
+      var accessToken = authHeader.replace('Bearer ', '')
       if (userId) {
         req.userId = userId
+        var _key = 'req_signature-' + userId
+        /**
+         * Signing Composr API Requests
+         */
+        if (config.get('signRequests.active'){
+          redisConnector.set(_key, accessToken, _durationInSeconds)
+        }
+
         return next()
       } else {
         return next(new ComposrError('unauthorized_token', 'Only users can perform this action', 401))
@@ -29,6 +46,9 @@ module.exports.authUser = function () {
   }
 }
 
+/**
+ * Auth client middleware
+ */
 module.exports.authClient = function () {
   return function authClient (req, res, next) {
     var authHeader = req.header('Authorization')
